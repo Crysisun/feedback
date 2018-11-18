8ce07958fd83bd6767d593add49c6ed592df6800
Crysisun <sunqingyu1@gmail.com>
2018-11-18 12:15:24 -0800

    Initial commit.

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ Hi: 'there'});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
