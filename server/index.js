a7b48094757ee79cd52dfd51628a02130545ef9b
Crysisun <sunqingyu1@gmail.com>
2018-11-18 12:45:59 -0800

    changed message

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ Hi: 'there'});
+    res.send({ Hi: 'haha'});
 });
 
 const PORT = process.env.PORT || 3000;
