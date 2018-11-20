d8e89399634f44a32d60bcaa95300a4255419d2d
Crysisun <sunqingyu1@gmail.com>
2018-11-19 19:43:46 -0800

    Implementing authentication with Google OAuth 2.0 with passport.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ Hi: 'haha'});
-});
+authRoutes(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
+
