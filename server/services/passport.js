9a72a02a0afb95e16cb8cead3f468a2477ed1384
Crysisun <sunqingyu1@gmail.com>
2018-11-20 17:58:57 -0800

    Implementing callback functions in passport.js along with the promises.

--- server/services/passport.js
+++ server/services/passport.js
@@ -10,7 +10,14 @@ passport.use(new GoogleStrategy({
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log(profile);
-        new User({ googleId: profile.id }).save();
+        User.findOne({ googleId: profile.id })
+            .then(existingUser => {
+                if (existingUser) {
+                    done(null, existingUser);
+                } else {
+                    new User({ googleId: profile.id }).save()
+                        .then(user => done(null, user));
+                }
+            });
     })
 );
