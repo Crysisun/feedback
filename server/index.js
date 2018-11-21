bacd8510f8bc093d7980e3ae4359e3d9c69134ba
Crysisun <sunqingyu1@gmail.com>
2018-11-20 16:43:53 -0800

    Setup MongoDB and connect mongoose to MongoDB.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
+require('./services/passport');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
