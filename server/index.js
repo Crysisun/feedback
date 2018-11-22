7ee1dbd749408969e291d24d484b5e6ee33ab364
Crysisun <sunqingyu1@gmail.com>
2018-11-21 23:51:59 -0800

    Setup cookieSession.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models/User');
 require('./services/passport');
 
@@ -9,6 +11,13 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+  })
+);
 
 authRoutes(app);
 
