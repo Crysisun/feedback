d36a0f03c9aa36162274b50172aae538078e98cd
Crysisun <sunqingyu1@gmail.com>
2018-11-24 23:56:53 -0800

    Adds logout route.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,6 +10,10 @@ module.exports = (app) => {
 
     app.get('/auth/google/callback', passport.authenticate('google'));
 
+    app.get('/api/logout', (req, res) => {
+       req.logout();
+    });
+
     app.get('/api/current_user', (req, res) => {
         res.send(req.user);
     });
