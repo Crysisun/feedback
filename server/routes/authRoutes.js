1d518713b9533c5882a24c34c4c65f2064cb2652
Crysisun <sunqingyu1@gmail.com>
2018-11-25 23:47:02 -0800

    Updates logout function

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -12,6 +12,7 @@ module.exports = (app) => {
 
     app.get('/api/logout', (req, res) => {
        req.logout();
+       res.send(req.user);
     });
 
     app.get('/api/current_user', (req, res) => {
