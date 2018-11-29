875e512265b97255fd381f9f953c70553e13ddcd
Crysisun <sunqingyu1@gmail.com>
2018-11-28 17:45:57 -0800

    test2

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '153209218424-2di7oetmu5ngpv5h4r4792lq6p1hjd2l.apps.googleusercontent.com',
+    googleClientSecret: 'bB-jliYkyBo-SLAcFey31CNO',
+    mongoURI: 'mongodb://jake:password1@ds039737.mlab.com:39737/feedback_dev',
+    cookieKey: 'lkjahsdfalfsdkasdkfh'
+};
