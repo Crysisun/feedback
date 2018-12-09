f805a9e7ae1f3e1c94690d2c885d8fe893148e93
Crysisun <sunqingyu1@gmail.com>
2018-12-09 00:48:48 -0800

    Connects action creator from redux with react, refractor on actions.

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -9,7 +9,7 @@ import App from './components/App';
 import reducers from './reducers';
 
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 
 ReactDOM.render(
