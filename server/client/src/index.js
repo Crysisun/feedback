39271a9d2553c3933ce6cb0f44025b6cc65676de
Crysisun <sunqingyu1@gmail.com>
2018-12-08 23:59:40 -0800

    Implementing action creators.

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+
 
 const store = createStore(reducers, {}, applyMiddleware());
 
