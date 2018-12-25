053e51367337ddc0b747cd35bd0a1e569fe58c88
Crysisun <sunqingyu1@gmail.com>
2018-12-24 23:55:33 -0800

    Dealing with adding credits on users account

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,12 +2,18 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
-    app.post('/api/stripe', (req, res) => {
-        stripe.charges.create({
+    app.post('/api/stripe', async (req, res) => {
+        if (!req.user) {
+            return res.status(401).send({ error: 'You must log in!'});
+        }
+        const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for 5 credits',
             source: req.body.id
-        })
+        });
+        req.user.credits += 5;
+        const user = await req.user.save();
+        res.send(user);
     });
 };
