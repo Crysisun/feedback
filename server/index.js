f5125714067a7e233a094ea6a213431312dc2717
Crysisun <sunqingyu1@gmail.com>
2018-12-27 23:54:00 -0800

    Setup routing in production environment.

--- server/index.js
+++ server/index.js
@@ -30,6 +30,19 @@ app.use(passport.session());
 authRoutes(app);
 billingRoutes(app);
 
+if (process.env.NODE_ENV === 'production') {
+    //Express will serve up production assets such as
+    //main.js and main.css files
+    app.use(express.static('client/build'));
+
+    //Express will serve up the index.html if it doesn't
+    //recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.solve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
