f8c6882dea201e7b623026945a801810a0c3f92c
Crysisun <sunqingyu1@gmail.com>
2019-01-09 23:42:10 -0800

    Fix bugs.

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -3,14 +3,14 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
-    constructor({ subject, recipient }, content) {
+    constructor({ subject, recipients }, content) {
         super();
 
         this.sgApi = sendgrid(keys.sendGridKey);
         this.from_email = new helper.Email('no-reply@feedback.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
-        this.recipients = this.formatAddresses(recipient);
+        this.recipients = this.formatAddresses(recipients);
 
         this.addContent(this.body);
         this.addClickTracking();
@@ -19,7 +19,7 @@ class Mailer extends helper.Mail {
     formatAddresses(recipients) {
         return recipients.map(({ email }) => {
             return new helper.Email(email);
-        })
+        });
     }
     addClickTracking() {
         const trackingSettings = new helper.TrackingSettings();
