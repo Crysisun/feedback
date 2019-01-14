708fcec57f6c712c022c7f3ea0e35aa186c90c61
Crysisun <sunqingyu1@gmail.com>
2019-01-14 11:56:44 -0800

    Implementing survey form.

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -5,7 +5,7 @@ import * as actions from '../actions';
 
 import Landing from './Landing';
 import Header from './Header';
-const Dashboard = () => <h2>Dashboard</h2>
+import Dashboard from './Dashboard'
 const SurveyNew = () => <h2>SurveyNew</h2>
 
 
