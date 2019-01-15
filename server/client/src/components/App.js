cca382ae371500192f559289bfb1642206f7981c
Crysisun <sunqingyu1@gmail.com>
2019-01-14 17:42:15 -0800

    Form validations.

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -6,7 +6,7 @@ import * as actions from '../actions';
 import Landing from './Landing';
 import Header from './Header';
 import Dashboard from './Dashboard'
-const SurveyNew = () => <h2>SurveyNew</h2>
+import SurveyNew from './surveys/SurveyNew';
 
 
 class App extends Component {
---
+++ server/client/src/components/surveys/SurveyField.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default ({ input, label, meta: { error, touched } }) => {
+    
+    return (
+        <div>
+            <label>{label}</label>
+            <input {...input}/>
+            { touched && error }
+        </div>
+    );
+};
