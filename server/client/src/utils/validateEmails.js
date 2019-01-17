41e216c82db89819b264810ec49bffbb6b2a9742
Crysisun <sunqingyu1@gmail.com>
2019-01-16 23:50:32 -0800

    Shows invalid emails.

--- server/client/src/utils/validateEmails.js
+++ server/client/src/utils/validateEmails.js
@@ -1,5 +1,12 @@
+const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export default (emails) => {
-    const emailsArray = emails
+    const invalidEmails = emails
         .split(',')
-        .map(email => email.trim());
+        .map(email => email.trim())
+        .filter(email => reg.test(reg) === false); //return the invalid ones
+    if (invalidEmails.length) {
+        return `These emails are invalid: ${invalidEmails}`;
+    }
+    return;
 };
