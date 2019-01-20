74c4e14d2bad5cd2acf27811f4faca845d01eb12
Crysisun <sunqingyu1@gmail.com>
2019-01-19 23:57:44 -0800

    Advancing from SurveyForm.

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 //SurveyNew shows SurveyForm and SurveyFormReview
 class SurveyNew extends Component {
