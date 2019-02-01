e9de704d6efa1eff2ecd4a2cd07b1534cbef5d1e
Crysisun <sunqingyu1@gmail.com>
2019-01-31 21:31:20 -0800

    Renders survey list.

--- server/client/src/reducers/surveysReducer.js
+++ server/client/src/reducers/surveysReducer.js
@@ -1,7 +1,7 @@
 import { FETCH_SURVEYS } from "../actions/types";
 
 export default function(state = [], action) {
-    switch (type) {
+    switch (action.type) {
         case FETCH_SURVEYS:
             return action.payload;
         default:
